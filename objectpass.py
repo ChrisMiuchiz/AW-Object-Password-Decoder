be7267a4832d1ac66e8303e983e65a165b0166c7
ChrisMiuchiz <chris000005@gmail.com>
2018-09-11 13:04:47 -0400

    Add files via upload

--- objectpass.py
+++ objectpass.py
@@ -3,28 +3,28 @@ def DeobfuscateObjectPassword(objpass):
     newpass = ''
     for e in objpass:
         e = ord(e) ^ sub_472580()
-        sub_472520(e)
+        Update(e)
         newpass += chr(e)
     return newpass
 
 def sub_472580():
     return (((((dword_764E8C | 2) & 0xFFFF) ^ 1) * ((dword_764E8C | 2) & 0xFFFF)) & 0xFFFF) >> 8
 
-def sub_472520(a1):
-    global dword_764E84, dword_764E88, dword_764E8C, dword_743518
+def Update(a1):
+    global dword_764E84, dword_764E88, dword_764E8C, crc_table
     
-    dword_764E84 = (dword_764E84>>8)^dword_743518[(dword_764E84^a1) & 0xFF]
+    dword_764E84 = (dword_764E84>>8)^crc_table[(dword_764E84^a1) & 0xFF]
     dword_764E88 = ((((dword_764E84 & 0xFF) + dword_764E88)*0x8088405) +1) & 0xFFFFFFFF
-    dword_764E8C = (dword_764E8C >> 8)^dword_743518[((dword_764E88 >> 0x18)^dword_764E8C)& 0xFF]
+    dword_764E8C = (dword_764E8C >> 8)^crc_table[((dword_764E88 >> 0x18)^dword_764E8C)& 0xFF]
     return a1
 
 def reset():
-    global dword_764E84, dword_764E88, dword_764E8C, dword_743518
+    global dword_764E84, dword_764E88, dword_764E8C, crc_table
     
     dword_764E84 = 0x15FC6BC5
     dword_764E88 = 0xAB0DD7D1
     dword_764E8C = 0xFE6CDE6C
-    dword_743518 = [0x00000000, 0x77073096, 0xEE0E612C, 0x990951BA, 0x076DC419, 0x706AF48F, 0xE963A535, 0x9E6495A3,
+    crc_table = [0x00000000, 0x77073096, 0xEE0E612C, 0x990951BA, 0x076DC419, 0x706AF48F, 0xE963A535, 0x9E6495A3,
                     0x0EDB8832, 0x79DCB8A4, 0xE0D5E91E, 0x97D2D988, 0x09B64C2B, 0x7EB17CBD, 0xE7B82D07, 0x90BF1D91,
                     0x1DB71064, 0x6AB020F2, 0xF3B97148, 0x84BE41DE, 0x1ADAD47D, 0x6DDDE4EB, 0xF4D4B551, 0x83D385C7,
                     0x136C9856, 0x646BA8C0, 0xFD62F97A, 0x8A65C9EC, 0x14015C4F, 0x63066CD9, 0xFA0F3D63, 0x8D080DF5,
